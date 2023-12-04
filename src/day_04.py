12dabb3307b88f1be72f92e5a554373571ad047a
Ben Gosney <bengosney@googlemail.com>
2023-12-04 12:38:32 +0000

    recursion baby!

--- src/day_04.py
+++ src/day_04.py
@@ -2,17 +2,18 @@
 import re
 from collections.abc import Iterable
 from dataclasses import dataclass
+from functools import lru_cache
 from typing import Self
 
 # First Party
 from utils import no_input_skip, read_input
 
 
-@dataclass
+@dataclass(frozen=True)
 class Card:
     id: int
-    numbers: list[int]
-    winning: list[int]
+    numbers: frozenset[int]
+    winning: frozenset[int]
 
     @classmethod
     def parse(cls, line: str) -> Self:
@@ -21,8 +22,8 @@ class Card:
         ours, winning = numbers.split("|")
         return Card(
             id=int(id),
-            numbers=list(map(int, ours.split())),
-            winning=list(map(int, winning.split())),
+            numbers=frozenset(map(int, ours.split())),
+            winning=frozenset(map(int, winning.split())),
         )
 
 
@@ -45,7 +46,26 @@ def part_1(input: str) -> int:
 
 
 def part_2(input: str) -> int:
-    pass
+    cards = list(parse(input))
+    card_count = 0
+
+    @lru_cache
+    def count_cards(card: Card) -> int:
+        wins = 0
+        count = 1
+        for number in card.numbers:
+            if number in card.winning:
+                wins += 1
+
+        for i in range(card.id, card.id + wins):
+            count += count_cards(cards[i])
+
+        return count
+
+    for card in cards:
+        card_count += count_cards(card)
+
+    return card_count
 
 
 # -- Tests
@@ -65,9 +85,9 @@ def test_part_1():
     assert part_1(test_input) == 13
 
 
-# def test_part_2():
-#    test_input = get_example_input()
-#    assert part_2(test_input) == 30
+def test_part_2():
+    test_input = get_example_input()
+    assert part_2(test_input) == 30
 
 
 @no_input_skip
@@ -76,10 +96,10 @@ def test_part_1_real():
     assert part_1(real_input) == 23673
 
 
-# @no_input_skip
-# def test_part_2_real():
-#     real_input = read_input(__file__)
-#     assert part_2(real_input) is not None
+@no_input_skip
+def test_part_2_real():
+    real_input = read_input(__file__)
+    assert part_2(real_input) == 12263631
 
 
 # -- Main
