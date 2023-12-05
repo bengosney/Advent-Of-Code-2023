8ee187483fd6b30ab0f4fd63c3e755bd66e82e23
Ben Gosney <bengosney@googlemail.com>
2023-12-05 13:36:48 +0000

    Part 2 doesnt work

--- src/day_05.py
+++ src/day_05.py
@@ -1,5 +1,11 @@
+# Standard Library
+from itertools import batched
+
 # First Party
 from utils import no_input_skip, read_input
+
+# Third Party
+from icecream import ic
 
 
 def part_1(input: str) -> int:
@@ -24,8 +30,40 @@ def part_1(input: str) -> int:
     return min(results)
 
 
+Block = tuple[int, int]
+
+
 def part_2(input: str) -> int:
-    pass
+    lines = input.splitlines()
+    seeds = list(map(int, lines[0].split()[1:]))
+
+    def split(a: Block, b: Block) -> list[Block]:
+        if a[1] <= b[0] or a[0] >= b[1]:
+            return [a]
+
+        if a[0] >= b[0] and a[1] <= b[1]:
+            return [a]
+
+        if b[0] > a[0] and b[0] < a[1]:
+            return [(a[0], b[0]), (b[0], a[1])]
+
+        if b[1] > a[0] and b[1] < a[1]:
+            return [(a[0], b[1]), (b[1], a[1])]
+
+        raise Exception(f"I got some logic wrong... {a} - {b}")
+
+    results = []
+    seed_blocks: list[Block] = []
+    for start, count in batched(seeds, 2):
+        seed_blocks.append((start, start + count))
+
+        for seed_block in seed_blocks:
+            tmp = split(seed_block, (50, 85))
+            ic(tmp)
+
+    ic(seed_blocks)
+
+    return min(results)
 
 
 # -- Tests
@@ -72,9 +110,9 @@ def test_part_1():
     assert part_1(test_input) == 35
 
 
-# def test_part_2():
-#     test_input = get_example_input()
-#     assert part_2(test_input) is not None
+def test_part_2():
+    test_input = get_example_input()
+    assert part_2(test_input) == 46
 
 
 @no_input_skip
@@ -94,5 +132,6 @@ def test_part_1_real():
 if __name__ == "__main__":
     real_input = read_input(__file__)
 
+    part_2(get_example_input())
     print(f"Part1: {part_1(real_input)}")
     print(f"Part2: {part_2(real_input)}")
