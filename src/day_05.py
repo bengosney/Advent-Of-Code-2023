d5cc95cb17eebd02f67933ec67fd166d8df827d0
Ben Gosney <bengosney@googlemail.com>
2023-12-08 18:50:01 +0000

    Day 5 part 2 without bruteforce

--- src/day_05.py
+++ src/day_05.py
@@ -1,6 +1,5 @@
 # Standard Library
 from collections.abc import Generator
-from functools import partial
 from itertools import batched
 
 # First Party
@@ -53,7 +52,7 @@ def split(a: Block, b: Block) -> Generator[Block, None, None]:
             yield splitting
 
 
-def part_2_no_work(input: str) -> int:
+def part_2(input: str) -> int:
     lines = input.splitlines()
     seeds = list(map(int, lines[0].split()[1:]))
 
@@ -62,92 +61,28 @@ def part_2_no_work(input: str) -> int:
     for start, count in batched(seeds, 2):
         seed_blocks.append((start, start + count))
 
-    def process_block(block: Block, start_line: int = 2) -> int:
-        found = False
+    def process_block(block: Block, start_line: int = 2, found: bool = False) -> int:
+        _found = found
         for i, line in enumerate(lines[start_line:]):
             if line == "" or ":" in line:
-                found = False
+                _found = False
                 if line != "":
                     pass
                 continue
             destination, source, length = list(map(int, line.split()))
             split_blocks = list(split(block, (source, source + length)))
             if len(split_blocks) > 1:
-                return min(process_block(b, i + start_line) for b in split_blocks)
-            if not found and block[0] >= source and block[1] <= source + length:
+                return min(process_block(b, i + start_line, _found) for b in split_blocks)
+            if not _found and block[0] >= source and block[1] <= source + length:
                 block = block[0] + (destination - source), block[1] + (destination - source)
-                found = True
+                _found = True
 
         return min(block)
 
-    for seed_block in seed_blocks[1:]:
+    for seed_block in seed_blocks:
         results.append(process_block(seed_block))
 
     return min(results)
-
-
-def try_location(i: int, lines, seed_blocks) -> int:
-    def is_seed(potential: int) -> bool:
-        for block in seed_blocks:
-            if potential >= block[0] and potential <= block[1]:
-                return True
-        return False
-
-    seed = i
-    found = False
-    for line in lines[:-1]:
-        if line == "" or ":" in line:
-            found = False
-            continue
-
-        source, destination, length = list(map(int, line.split()))
-        if not found and seed >= source and seed <= source + length:
-            seed += destination - source
-            found = True
-
-    if is_seed(seed):
-        print(f"-- {i} --")
-        return i
-    return 0
-    return i if is_seed(seed) else 0
-
-
-def wtf(value: int):
-    with open("day5_state.txt", "w") as f:
-        f.write(str(value))
-
-
-def rtf() -> int:
-    try:
-        with open("day5_state.txt") as f:
-            return int(f.readline())
-    except FileNotFoundError:
-        return 0
-
-
-def write_ans(value):
-    with open("day5_ans.txt", "w") as f:
-        f.write(str(value))
-
-
-def part_2(input: str) -> int:
-    lines = "\n".join(input.split("\n\n")[::-1]).splitlines()
-
-    seed_blocks: list[Block] = []
-    for start, count in batched(list(map(int, lines[-1].split()[1:])), 2):
-        seed_blocks.append((start, start + count))
-
-    start = 0
-    try_loc = partial(try_location, lines=lines, seed_blocks=seed_blocks)
-
-    start = 9611000
-    start = 9610999
-    for i in range(start, 11_651_122):
-        # print(f"{i} - {i + 1000}")
-        if ans := try_loc(i):
-            return ans
-
-    raise Exception("I hate this puzzle")
 
 
 # -- Tests
@@ -227,10 +162,7 @@ def test_part_1_real():
 @no_input_skip
 def test_part_2_real():
     real_input = read_input(__file__)
-    ans = part_2(real_input)
-    assert ans < 11651122
-    assert ans != 9622623
-    assert ans == 9622622
+    assert part_2(real_input) == 9622622
 
 
 # -- Main
@@ -238,6 +170,5 @@ def test_part_2_real():
 if __name__ == "__main__":
     real_input = read_input(__file__)
 
-    # part_2(get_example_input())
-    # print(f"Part1: {part_1(real_input)}")
+    print(f"Part1: {part_1(real_input)}")
     print(f"Part2: {part_2(real_input)}")
