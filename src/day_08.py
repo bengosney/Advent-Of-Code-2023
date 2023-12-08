c3f8187d8a5e6b513b2d7d9654e118c6d2d785ae
Ben Gosney <bengosney@googlemail.com>
2023-12-08 15:32:57 +0000

    Day 8

--- src/day_08.py
+++ src/day_08.py
@@ -1,50 +1,63 @@
 # Standard Library
 import re
-from dataclasses import dataclass
+from collections.abc import Callable
+from functools import partial
 from itertools import cycle
+from math import lcm
+from typing import Literal
 
 # First Party
-from utils import no_input_skip, read_input  # noqa
+from utils import no_input_skip, read_input
+
+Node = dict[Literal["L", "R"], str]
+Instructions = list[Literal["L", "R"]]
 
 
-@dataclass(frozen=True)
-class Node:
-    name: str
-    left: str
-    right: str
+def solve(current: str, targets: list[str], nodes: dict[str, Node], instructions: Instructions) -> int:
+    for i, dir in enumerate(cycle(instructions)):
+        if current in targets:
+            return i
+        current = nodes[current][dir]
+
+    raise Exception("This can not happen")
+
+
+def parse(input: str) -> tuple[Instructions, dict[str, Node]]:
+    instruction_string, node_strings = input.split("\n\n")
+    regex = re.compile(r"(\w{3})\s=\s\((\w{3}),\s(\w{3})\)")
+    nodes: dict[str, Node] = {}
+    for string in node_strings.splitlines():
+        if match := regex.match(string):
+            nodes[match.group(1)] = {"L": match.group(2), "R": match.group(3)}
+    instructions: Instructions = [i for i in instruction_string if i == "R" or i == "L"]
+
+    return instructions, nodes
+
+
+def get_solver(instructions: Instructions, nodes: dict[str, Node], targets: list[str]) -> Callable[[str], int]:
+    return partial(solve, instructions=instructions, nodes=nodes, targets=targets)
 
 
 def part_1(input: str) -> int:
-    instructions, node_strings = input.split("\n\n")
-    regex = re.compile(r"(\w{3})\s=\s\((\w{3}),\s(\w{3})\)")
-    nodes = {}
-    for string in node_strings.splitlines():
-        if match := regex.match(string):
-            node = Node(*match.groups())
-            nodes[node.name] = node
+    instructions, nodes = parse(input)
 
-    steps = 0
-    current = "AAA"
-    for i in cycle(instructions):
-        if i == "L":
-            current = nodes[current].left
-        else:
-            current = nodes[current].right
-        steps += 1
-        if current == "ZZZ":
-            break
-
-    return steps
+    return solve("AAA", ["ZZZ"], nodes, instructions)
 
 
 def part_2(input: str) -> int:
-    pass
+    instructions, nodes = parse(input)
+
+    starts = [n for n in nodes if n[-1] == "A"]
+    targets = [n for n in nodes if n[-1] == "Z"]
+    solver = get_solver(instructions, nodes, targets)
+
+    return lcm(*[solver(c) for c in starts])
 
 
 # -- Tests
 
 
-def get_example_input() -> str:
+def get_example_input_one() -> str:
     return """RL
 
 AAA = (BBB, CCC)
@@ -56,26 +69,40 @@ GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)"""
 
 
+def get_example_input_two() -> str:
+    return """LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+"""
+
+
 def test_part_1():
-    test_input = get_example_input()
+    test_input = get_example_input_one()
     assert part_1(test_input) == 2
 
 
-# def test_part_2():
-#     test_input = get_example_input()
-#     assert part_2(test_input) is not None
+def test_part_2():
+    test_input = get_example_input_two()
+    assert part_2(test_input) == 6
 
 
-# @no_input_skip
-# def test_part_1_real():
-#     real_input = read_input(__file__)
-#     assert part_1(real_input) is not None
+@no_input_skip
+def test_part_1_real():
+    real_input = read_input(__file__)
+    assert part_1(real_input) == 21797
 
 
-# @no_input_skip
-# def test_part_2_real():
-#     real_input = read_input(__file__)
-#     assert part_2(real_input) is not None
+@no_input_skip
+def test_part_2_real():
+    real_input = read_input(__file__)
+    assert part_2(real_input) == 23977527174353
 
 
 # -- Main
