b6dcce0fc3b57d439a5a6abb87c7b51f8f656811
Ben Gosney <bengosney@googlemail.com>
2023-12-13 15:43:59 +0000

    Day 13 part 2 - WIP

--- src/day_13.py
+++ src/day_13.py
@@ -19,6 +19,27 @@ def reflect(rows: list[str]) -> int:
     return 0
 
 
+def reflect_smudg(rows: list[str], error_tolerance: int = 0) -> int:
+    length = len(rows) - 1
+    for i in range(length):
+        p = min(length - i, i)
+        m = 0 if (length - i) > i else 1
+        a = set(rows[(i - p) + m : i + 1])
+        b = set(rows[i + 1 : i + p + 2][::-1])
+
+        if len(a - b) == 0:
+            return i + 1
+        if len(a - b) == 1:
+            left, right = list(a - b), list(b - a)
+            diff = 0
+            for i in range(len(left[0])):
+                if left[0][i] != right[0][i]:
+                    diff += 1
+            if diff <= 1:
+                return i + 1
+    return 0
+
+
 def part_1(input: str) -> int:
     puzzles = input.split("\n\n")
     answer = 0
@@ -32,7 +53,15 @@ def part_1(input: str) -> int:
 
 
 def part_2(input: str) -> int:
-    pass
+    puzzles = input.split("\n\n")
+    answer = 0
+    for puzzle in puzzles:
+        cols = rotate(puzzle).splitlines()
+        rows = puzzle.splitlines()
+
+        answer += reflect_smudg(cols, 1) + (reflect_smudg(rows, 1) * 100)
+
+    return answer
 
 
 # -- Tests
@@ -61,9 +90,9 @@ def test_part_1():
     assert part_1(test_input) == 405
 
 
-# def test_part_2():
-#    test_input = get_example_input()
-#    assert part_2(test_input) == 400
+def test_part_2():
+    test_input = get_example_input()
+    assert part_2(test_input) == 400
 
 
 @no_input_skip
@@ -72,10 +101,10 @@ def test_part_1_real():
     assert part_1(real_input) == 39939
 
 
-# @no_input_skip
-# def test_part_2_real():
-#     real_input = read_input(__file__)
-#     assert part_2(real_input) is not None
+@no_input_skip
+def test_part_2_real():
+    real_input = read_input(__file__)
+    assert part_2(real_input) > 10409
 
 
 # -- Main
@@ -83,5 +112,6 @@ def test_part_1_real():
 if __name__ == "__main__":
     real_input = read_input(__file__)
 
+    test_part_2()
     print(f"Part1: {part_1(real_input)}")
     print(f"Part2: {part_2(real_input)}")
