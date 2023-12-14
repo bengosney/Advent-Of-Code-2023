d609c69870e2af8a5bdbab341a18ce7a7250cd7e
Ben Gosney <bengosney@googlemail.com>
2023-12-14 21:20:40 +0000

    Day 14 part 2, it's ugly, it's slow but it works!

--- src/day_14.py
+++ src/day_14.py
@@ -3,22 +3,46 @@ from collections import defaultdict
 from enum import Enum
 
 # First Party
-from utils import no_input_skip, read_input, time_limit
+from utils import no_input_skip, read_input
 
-# Third Party
-from icecream import ic
+
+def parse(input: str) -> tuple[dict[tuple[int, int], str], int, int]:
+    platform: dict[tuple[int, int], str] = defaultdict(lambda: "!")
+    width = 0
+    height = 0
+    for y, row in enumerate(input.splitlines()):
+        height = y + 1
+        for x, thing in enumerate(row):
+            width = max(width, x + 1)
+            platform[x, y] = thing
+    return platform, width, height
 
 
 class Directions(Enum):
     NORTH = (0, -1)
-    EAST = (1, 0)
-    SOUTH = (0, 1)
     WEST = (-1, 0)
+    SOUTH = (0, 1)
+    EAST = (1, 0)
 
 
 def roll(platform: dict[tuple[int, int], str], dir: tuple[int, int], width: int, height: int) -> dict[tuple[int, int], str]:
-    for y in range(height):
-        for x in range(width):
+    match dir:
+        case (0, -1):  # North
+            y_range = range(height)
+            x_range = range(width)
+        case (-1, 0):  # West
+            y_range = range(height)
+            x_range = range(width)
+        case (0, 1):  # South
+            y_range = range(height, -1, -1)
+            x_range = range(width)
+        case (1, 0):  # East
+            y_range = range(height)
+            x_range = range(width, -1, -1)
+        case _:
+            raise Exception(f"What dir is this? {dir}")
+    for y in y_range:
+        for x in x_range:
             if platform[x, y] == "O":
                 rx, ry = x, y
                 while platform[rx + dir[0], ry + dir[1]] == ".":
@@ -30,15 +54,7 @@ def roll(platform: dict[tuple[int, int], str], dir: tuple[int, int], width: int,
 
 
 def part_1(input: str) -> int:
-    platform: dict[tuple[int, int], str] = defaultdict(lambda: "!")
-    width = 0
-    height = 0
-    for y, row in enumerate(input.splitlines()):
-        height = y + 1
-        for x, thing in enumerate(row):
-            width = max(width, x + 1)
-            platform[x, y] = thing
-
+    platform, width, height = parse(input)
     platform = roll(platform, (0, -1), width, height)
 
     weight = 0
@@ -51,19 +67,21 @@ def hash_platform(platform: dict[tuple[int, int], str]) -> str:
     return "".join(platform.values())
 
 
-def part_2(input: str) -> int:
-    platform: dict[tuple[int, int], str] = defaultdict(lambda: "!")
-    width = 0
-    height = 0
-    for y, row in enumerate(input.splitlines()):
-        height = y + 1
-        for x, thing in enumerate(row):
-            width = max(width, x + 1)
-            platform[x, y] = thing
+def cycles(platform: dict[tuple[int, int], str], rounds: int, width: int, height: int) -> dict[tuple[int, int], str]:
+    def roll_dirs(platform: dict[tuple[int, int], str]) -> dict[tuple[int, int], str]:
+        for dir in Directions:
+            platform = roll(platform, dir.value, width, height)
+        return platform
+
+    for _ in range(rounds):
+        platform = roll_dirs(platform)
+    return platform
+
+
+def part_2(input: str, rounds: int = 1_000_000_000) -> int:
+    platform, width, height = parse(input)
 
     cache: dict[str, int] = {}
-
-    rounds = 1_000_000_000
 
     def roll_dirs(platform: dict[tuple[int, int], str]) -> dict[tuple[int, int], str]:
         for dir in Directions:
@@ -71,8 +89,8 @@ def part_2(input: str) -> int:
         return platform
 
     for i in range(rounds):
-        ic("round", i)
         platform = roll_dirs(platform)
+
         platform_hash = hash_platform(platform)
         if platform_hash not in cache:
             cache[platform_hash] = i
@@ -81,11 +99,10 @@ def part_2(input: str) -> int:
             idx = cache[platform_hash] + (rounds - cache[platform_hash]) % cycle
             search_hash = ""
             for item in cache.items():
-                if item[1] == idx:
+                if item[1] == idx - 1:
                     search_hash = item[0]
             while hash_platform(platform) != search_hash:
-                for _ in range(rounds % idx):
-                    platform = roll_dirs(platform)
+                platform = roll_dirs(platform)
             break
 
     weight = 0
@@ -115,10 +132,47 @@ def test_part_1():
     assert part_1(test_input) == 136
 
 
+def build_grid(
+    grid: dict[tuple[int, int], str], start: tuple[int, int] | None = None, end: tuple[int, int] | None = None
+) -> str:
+    _start = start or min(grid)
+    _end = end or max(grid)
+    rows: list[str] = []
+    for y in range(_start[1], _end[1]):
+        row: str = ""
+        for x in range(_start[0], _end[0]):
+            row += grid[(x, y)]
+        rows.append(row)
+    return "\n".join(rows)
+
+
+def test_cycle():
+    test_input = get_example_input()
+    platform, width, height = parse(test_input)
+    result = build_grid(cycles(platform, 1, width, height), (0, 0), (width, height))
+
+    answer = """.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#...."""
+
+    assert result == answer
+
+
 def test_part_2():
     test_input = get_example_input()
-    with time_limit(10):
-        assert part_2(test_input) == 64
+    assert part_2(test_input) == 64
+
+
+def test_part_2_3_rounds():
+    test_input = get_example_input()
+    assert part_2(test_input, 3) == 69
 
 
 @no_input_skip
@@ -127,10 +181,10 @@ def test_part_1_real():
     assert part_1(real_input) == 105623
 
 
-# @no_input_skip
-# def test_part_2_real():
-#     real_input = read_input(__file__)
-#     assert part_2(real_input) is not None
+@no_input_skip
+def test_part_2_real():
+    real_input = read_input(__file__)
+    assert part_2(real_input) == 98029
 
 
 # -- Main
@@ -138,5 +192,7 @@ def test_part_1_real():
 if __name__ == "__main__":
     real_input = read_input(__file__)
 
-    # print(f"Part1: {part_1(real_input)}")
+    test_part_1()
+    test_cycle()
+    print(f"Part1: {part_1(real_input)}")
     print(f"Part2: {part_2(real_input)}")
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -5,4 +5,12 @@ from utils.decorators import no_input_skip
 from utils.helpers import ocr, read_input
 from utils.visualisers import GridType, draw_grid
 
-__all__ = ["read_input", "ocr", "no_input_skip", "draw_grid", "GridType", "CachingDict", "time_limit"]
+__all__ = [
+    "read_input",
+    "ocr",
+    "no_input_skip",
+    "draw_grid",
+    "GridType",
+    "CachingDict",
+    "time_limit",
+]
