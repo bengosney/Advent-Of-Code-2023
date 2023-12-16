31d7a51f8e285a274103d1127b5be6b25228bbfd
Ben Gosney <bengosney@googlemail.com>
2023-12-16 11:07:59 +0000

    Move it into a function

--- src/day_16.py
+++ src/day_16.py
@@ -1,5 +1,6 @@
 # Standard Library
 from collections import defaultdict, deque
+from functools import partial
 from typing import Deque
 
 # First Party
@@ -23,16 +24,11 @@ mirror_map_bs = {UP: LEFT, LEFT: UP, DOWN: RIGHT, RIGHT: DOWN}
 Beam = tuple[Vec2, Vec2]
 
 
-def part_1(input: str) -> int:
-    grid: dict[Vec2, str] = defaultdict(lambda: "!")
-    for y, line in enumerate(input.splitlines()):
-        for x, c in enumerate(line):
-            grid[x, y] = c
-
-    energized: dict[Vec2, str] = {}
+def energize(start: Vec2, direction: Vec2, grid: dict[Vec2, str]) -> int:
+    energized: set[Vec2] = set()
     history: set[tuple[Vec2, Vec2]] = set()
 
-    beams: Deque[Beam] = deque([((0, 0), RIGHT)])
+    beams: Deque[Beam] = deque([(start, direction)])
 
     while len(beams):
         current, direction = beams.pop()
@@ -56,13 +52,22 @@ def part_1(input: str) -> int:
             if grid[current] == "\\" and direction in mirror_map_bs:
                 direction = mirror_map_bs[direction]
 
-            energized[current] = "#"
+            energized.add(current)
             if grid[current] == ".":
                 grid[current] = "*"
 
             current = add(current, direction)
 
-    return list(energized.values()).count("#")
+    return len(energized)
+
+
+def part_1(input: str) -> int:
+    grid: dict[Vec2, str] = defaultdict(lambda: "!")
+    for y, line in enumerate(input.splitlines()):
+        for x, c in enumerate(line):
+            grid[x, y] = c
+
+    return energize((0, 0), RIGHT, grid)
 
 
 def part_2(input: str) -> int:
@@ -82,41 +87,9 @@ def part_2(input: str) -> int:
         starts.append(((0, y), RIGHT))
         starts.append(((max_x, y), LEFT))
 
+    _energize = partial(energize, grid=grid)
     for start in starts:
-        energized: dict[Vec2, str] = {}
-        history: set[tuple[Vec2, Vec2]] = set()
-
-        beams: Deque[Beam] = deque([start])
-
-        while len(beams):
-            current, direction = beams.pop()
-            while grid[current] != "!":
-                if grid[current] == "!" or (current, direction) in history:
-                    break
-
-                history.add((current, direction))
-
-                if grid[current] == "|" and direction in [LEFT, RIGHT]:
-                    beams.append((add(current, UP), UP))
-                    direction = DOWN
-
-                if grid[current] == "-" and direction in [UP, DOWN]:
-                    beams.append((add(current, LEFT), LEFT))
-                    direction = RIGHT
-
-                if grid[current] == "/" and direction in mirror_map_fs:
-                    direction = mirror_map_fs[direction]
-
-                if grid[current] == "\\" and direction in mirror_map_bs:
-                    direction = mirror_map_bs[direction]
-
-                energized[current] = "#"
-                if grid[current] == ".":
-                    grid[current] = "*"
-
-                current = add(current, direction)
-
-        scores.append(list(energized.values()).count("#"))
+        scores.append(_energize(*start))
 
     return max(scores)
 
